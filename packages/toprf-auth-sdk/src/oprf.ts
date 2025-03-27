12ee5ef6507efdf9015bd629e3b9463efda6e859
Matthias Geihs <matthias@geihs.de>
2025-03-27 17:33:45 +0100

    toprf: add documentation

--- packages/toprf-auth-sdk/src/oprf.ts
+++ packages/toprf-auth-sdk/src/oprf.ts
@@ -2,9 +2,6 @@
 // of PRFs.
 /* eslint-disable id-length */
 
-// TODO: add docs
-/* eslint-disable jsdoc/require-jsdoc */
-
 import { Field } from '@noble/curves/abstract/modular';
 import { bytesToNumberBE } from '@noble/curves/abstract/utils';
 import type { ProjPointType } from '@noble/curves/abstract/weierstrass';
@@ -21,7 +18,22 @@ export function generateRandomScalar(): bigint {
 
 const scalarField = Field(secp256k1.CURVE.n);
 
+/**
+ * (Threshold) OPRF (Oblivious Pseudorandom Function) class. Implements the
+ * 2HashDH OPRF from https://eprint.iacr.org/2016/144.pdf,
+ * https://eprint.iacr.org/2017/363.pdf.
+ */
 export class OPRF {
+  /**
+   * Blinds an input value by hashing it to an elliptic curve point and
+   * multiplying it by a random scalar. This process is used in cryptographic
+   * protocols to obscure the input value while preserving its structure.
+   *
+   * @param x - The input value as a `Uint8Array` to be blinded.
+   * @returns An object containing:
+   * - `a`: The blinded elliptic curve point as a projective point.
+   * - `r`: The random scalar used for blinding.
+   */
   public static blind(x: Uint8Array): { a: ProjPointType<bigint>; r: bigint } {
     const r = generateRandomScalar();
     const a = hashToCurve(x).multiply(r);
@@ -29,6 +41,14 @@ export class OPRF {
     return { a: aProjective, r };
   }
 
+  /**
+   * Evaluates the blinded input by multiplying the elliptic curve point by a
+   * scalar.
+   *
+   * @param k - The OPRF key, or key share.
+   * @param a - The blinded elliptic curve point as a projective point.
+   * @returns The blinded output as a projective point.
+   */
   public static blindEval(
     k: bigint,
     a: ProjPointType<bigint>,
@@ -37,6 +57,15 @@ export class OPRF {
     return b;
   }
 
+  /**
+   * Unblinds the output of the OPRF by multiplying the blinded output with the
+   * inverse of the blinding scalar and hashing it with the original input.
+   *
+   * @param x - The original input value as a `Uint8Array`.
+   * @param b - The blinded output as a projective point.
+   * @param r - The blinding scalar used for blinding.
+   * @returns The unblinded output as a `Uint8Array`.
+   */
   public static unblindAndHash(
     x: Uint8Array,
     b: ProjPointType<bigint>,
@@ -49,6 +78,13 @@ export class OPRF {
     return y;
   }
 
+  /**
+   * Performs local evaluation of the OPRF using a given key and input value.
+   *
+   * @param k - The OPRF key.
+   * @param x - The input value as a `Uint8Array` to be evaluated.
+   * @returns The evaluated output as a `Uint8Array`.
+   */
   public static localEval(k: bigint, x: Uint8Array): Uint8Array {
     // Blind input.
     const { a, r } = this.blind(x);
