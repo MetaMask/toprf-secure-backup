c287d43166a1c7ef02712330bf1ae0b3968879a5
himanshu <himanshuchawla2014@gmail.com>
2025-04-03 13:05:26 +0530

    simplified commit calls

--- packages/toprf-secure-backup/src/commitRequest.ts
+++ packages/toprf-secure-backup/src/commitRequest.ts
@@ -64,11 +64,11 @@ export const sendCommitmentRequest = async (
 };
 
 /**
- * Validates the commitment responses
+ * Validates the commitment responses.
  *
- * @param resultArr - The commitment request result
- * @param threshold - The threshold for the number commitment responses to be valid
- * @returns The commitment request result
+ * @param resultArr - The commitment request result.
+ * @param threshold - The threshold for the number commitment responses to be valid.
+ * @returns The commitment request result.
  */
 export const validateThresholdCommitmentResponses = async (
   resultArr: CommitmentJRPCResponse[],
@@ -86,19 +86,16 @@ export const validateThresholdCommitmentResponses = async (
     },
   );
 
-  if (completedRequests.length >= threshold) {
-    const requiredNodeResult = completedRequests.find(
-      (resp) => resp !== undefined && 'result' in resp,
+  if (completedRequests.length < threshold) {
+    return Promise.reject(
+      new Error(
+        `Not enough completed requests. Expected: ${threshold}, got: ${completedRequests.length}, ${JSON.stringify(resultArr)}`,
+      ),
     );
-    if (requiredNodeResult) {
-      const validResultArr = completedRequests.filter((res) => res.result);
-      return validResultArr.map((res) => res.result as CommitmentRequestResult);
-    }
   }
-
-  return Promise.reject(
-    new Error(`invalid commitment results ${JSON.stringify(resultArr)}`),
-  );
+  return completedRequests
+    .filter((res) => res.result)
+    .map((res) => res.result as CommitmentRequestResult);
 };
 
 /**
