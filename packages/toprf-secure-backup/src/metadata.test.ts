9d07b5884b722f826e752c2cf8788b574d724040
lwin <lwin@tor.us>
2025-04-04 16:02:16 +0800

    test: updated test for batchAddData and metadata lock

--- packages/toprf-secure-backup/src/metadata.test.ts
+++ packages/toprf-secure-backup/src/metadata.test.ts
@@ -5,7 +5,7 @@ import {
   deriveAuthenticationKeyPair,
   deriveEncryptionKey,
 } from './keyDerivation';
-import { MetadataStore } from './metadata';
+import { MetadataLockStatus, MetadataStore } from './metadata';
 import { generateMockAuthTokenForMetadataRequests } from '../tests/testHelpers';
 
 const MOCK_SEED = randomBytes(32);
@@ -99,6 +99,90 @@ describe('MetadataStore', () => {
     expect(result?.[0]).toStrictEqual(secretData);
   });
 
+  it('should be able to acquire and release metadata lock', async () => {
+    const metadataStore = createMockMetadataStore();
+
+    const metadataLock = await metadataStore.acquireMetadataLock(
+      authKeyPair,
+      nodeAuthTokens,
+    );
+
+    expect(metadataLock).not.toBeNull();
+
+    const releaseLockStatus = await metadataStore.releaseMetadataLock(
+      authKeyPair,
+      metadataLock,
+      nodeAuthTokens,
+    );
+
+    expect(releaseLockStatus).toStrictEqual(MetadataLockStatus.SUCCESS);
+  });
+
+  it('should fail to acquire lock if it is already acquired', async () => {
+    const metadataStore = createMockMetadataStore();
+
+    const metadataLock = await metadataStore.acquireMetadataLock(
+      authKeyPair,
+      nodeAuthTokens,
+    );
+    expect(metadataLock).toBeDefined();
+
+    await expect(
+      metadataStore.acquireMetadataLock(authKeyPair, nodeAuthTokens),
+    ).rejects.toThrow('Failed to acquire metadata lock');
+
+    // release the lock
+    const lockStatus = await metadataStore.releaseMetadataLock(
+      authKeyPair,
+      metadataLock,
+      nodeAuthTokens,
+    );
+    expect(lockStatus).toBe(MetadataLockStatus.SUCCESS);
+  });
+
+  it('should throw an error if lockId is missing in the response', async () => {
+    const fetchSpy = jest
+      .spyOn(global, 'fetch')
+      .mockImplementation(async () => {
+        return Promise.resolve({
+          ok: true,
+          status: 200,
+          statusText: 'OK',
+          /**
+           * @returns json object
+           */
+          json: async () => Promise.resolve({ status: 1 }),
+          // eslint-disable-next-line no-restricted-globals
+        } as Response);
+      });
+
+    const metadataStore = createMockMetadataStore();
+
+    await expect(
+      metadataStore.acquireMetadataLock(authKeyPair, nodeAuthTokens),
+    ).rejects.toThrow('Failed to acquire metadata lock. Missing lock id');
+
+    expect(fetchSpy).toHaveBeenCalled();
+    jest.restoreAllMocks();
+  });
+
+  it('should throw an error if the lock is not found for the specific nodeIndex (or Endpoint) during release', async () => {
+    const mockNodeEndpointsMap = new Map<number, string>([
+      [1, METADATA_SERVER_URL],
+      [2, METADATA_SERVER_URL],
+      [3, METADATA_SERVER_URL],
+    ]);
+    const metadataStore = createMockMetadataStore(mockNodeEndpointsMap);
+
+    await expect(
+      metadataStore.releaseMetadataLock(
+        authKeyPair,
+        [{ id: 'LOCK_ID_2', nodeIndex: 2 }],
+        nodeAuthTokens,
+      ),
+    ).rejects.toThrow('Could not find lock for node index 1');
+  });
+
   it('should be able to store secret data in batch', async () => {
     const metadataStore = createMockMetadataStore();
 
@@ -114,21 +198,31 @@ describe('MetadataStore', () => {
       encKey,
       authKeyPair,
     );
-
     expect(existingSecretData).not.toBeNull();
 
-    const newEncKey = deriveEncryptionKey(randomBytes(32));
+    // derive new encryption key and authentication key pair from the new seed
+    const newSeed = randomBytes(32);
+    const newEncKey = deriveEncryptionKey(newSeed);
+    const newAuthKeyPair = deriveAuthenticationKeyPair(newSeed);
+
+    // acquire the metadata lock
+    const metadataLock = await metadataStore.acquireMetadataLock(
+      newAuthKeyPair,
+      nodeAuthTokens,
+    );
+    expect(metadataLock).not.toBeNull();
+
     await metadataStore.batchAddSecretData({
       secretData: existingSecretData ?? [], // should not be null, the above `expect` should have failed if it was
       encKey: newEncKey,
-      authKeyPair,
+      authKeyPair: newAuthKeyPair,
       nodeAuthTokens,
     });
 
     // the result should be the new encrypted value of the existing secret data
     const newSecretData = await metadataStore.fetchAllSecretDataItems(
       newEncKey,
-      authKeyPair,
+      newAuthKeyPair,
     );
 
     expect(newSecretData).not.toBeNull();
@@ -137,6 +231,15 @@ describe('MetadataStore', () => {
     const sortedResult = newSecretData?.sort();
     expect(sortedResult?.[0]).toStrictEqual(existingSecretData?.[0]);
     expect(sortedResult?.[1]).toStrictEqual(existingSecretData?.[1]);
+
+    // release the metadata lock
+    const releaseLockStatus = await metadataStore.releaseMetadataLock(
+      newAuthKeyPair,
+      metadataLock,
+      nodeAuthTokens,
+    );
+
+    expect(releaseLockStatus).toStrictEqual(MetadataLockStatus.SUCCESS);
   });
 
   it('should get null if metadata key not found', async () => {
@@ -242,6 +345,31 @@ describe('MetadataStore', () => {
     await expect(
       metadataStore.fetchAllSecretDataItems(encKey, authKeyPair),
     ).rejects.toThrow('Threshold not resolved');
+
+    await expect(
+      metadataStore.acquireMetadataLock(authKeyPair, nodeAuthTokens),
+    ).rejects.toThrow('Something went wrong!');
+
+    await expect(
+      metadataStore.batchAddSecretData({
+        secretData: [utf8ToBytes('SECRET_DATA')],
+        encKey,
+        authKeyPair,
+        nodeAuthTokens,
+      }),
+    ).rejects.toThrow('Something went wrong!');
+
+    await expect(
+      metadataStore.releaseMetadataLock(
+        authKeyPair,
+        [
+          { id: '1', nodeIndex: 1 },
+          { id: '2', nodeIndex: 2 },
+          { id: '3', nodeIndex: 3 },
+        ],
+        nodeAuthTokens,
+      ),
+    ).rejects.toThrow('Something went wrong!');
 
     expect(fetchSpy).toHaveBeenCalled();
 
