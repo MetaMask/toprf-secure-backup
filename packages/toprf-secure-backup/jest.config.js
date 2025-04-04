0f4b8f02d37f30e49f0c66fe413ed77f2c925148
Matthias Geihs <matthias@geihs.de>
2025-04-04 16:24:06 +0200

    increase coverageThreshold because we can

--- packages/toprf-secure-backup/jest.config.js
+++ packages/toprf-secure-backup/jest.config.js
@@ -17,10 +17,16 @@ module.exports = merge(baseConfig, {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
+    // global: {
+    //   branches: 80,
+    //   functions: 80,
+    //   lines: 80,
+    //   statements: -10,
+    // },
   },
 });
