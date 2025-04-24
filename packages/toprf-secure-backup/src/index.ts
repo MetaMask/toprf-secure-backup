8d7d57a4e89dba97cf3a00be3561154aeac02098
himanshu <himanshuchawla2014@gmail.com>
2025-04-24 16:00:31 +0530

    export utils required for load testing

--- packages/toprf-secure-backup/src/index.ts
+++ packages/toprf-secure-backup/src/index.ts
@@ -2,3 +2,4 @@ export type * from './interfaces';
 export * from './toprfSecureBackup';
 export type { RateLimitErrorData } from './errors';
 export { TOPRFError, TOPRFErrorCode } from './errors';
+export * from './utils';
